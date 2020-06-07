5d1aabd33e5e297dafeaa1d979a01118544c51df
Noor khan <62239655+abdulnoorkhanwaleed@users.noreply.github.com>
2020-06-07 20:57:14 +0500

    updated for check

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div>
       <h1>Hello Noor Khan!!!!</h1>
+      <h2>My full Name Is Noor Khan Waleed</h2>
       <p>I am noor khan waleed i am muslim . i belongs to good family thankYou!!!!!</p>
     </div>
   );
